3dc673a09c46cd33f901fa21ee24bc0d28e52f02
celestedallago <celedallago@gmail.com>
2024-10-31 15:03:06 -0300

    Initial commit

---
+++ R/fahrenheit_a_centigrados.R
@@ -0,0 +1,17 @@
+
+#' Funcion para covertir grados fahrenheit a centigrados
+#'
+#' @param temperatura_f
+#'
+#' @return retorna el grado fahrenheit en centigrados
+#'
+#'
+#' @examples
+#' fahrenheit_a_celsius(98.6)
+#' Esta funcion deberia devolver 37
+#'
+#' @export
+fahrenheit_a_centigrados <- function(temperatura_f) {
+  temperatura_c <- (temperatura_f - 32) * 5 / 9
+  return(temperatura_c)
+}
