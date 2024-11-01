9aa49ed73e9843432fefc00344678ee20ba0f5ad
celestedallago <celedallago@gmail.com>
2024-11-01 00:11:59 -0300

    paso todos los test

--- tests/testthat/test-punto_rocio.R
+++ tests/testthat/test-punto_rocio.R
@@ -1,3 +1,19 @@
 test_that("punto de rocio", {
   expect_equal(object = punto_rocio(3), expected = "El punto de rocio es bajo; el aire se siente fresco y seco.")
 })
+
+test_that("punto de rocio", {
+  expect_equal(object = punto_rocio(-2), expected = "El punto de rocio es muy bajo; el aire es muy seco.")
+})
+
+test_that("punto de rocio", {
+  expect_equal(object = punto_rocio(12), expected = "El punto de rocio es moderado; las condiciones son comodas.")
+})
+
+test_that("punto de rocio", {
+  expect_equal(object = punto_rocio(16), expected = "El punto de rocio es alto; el aire se siente un poco humedo.")
+})
+
+test_that("punto de rocio", {
+  expect_equal(object = punto_rocio(30), expected = "El punto de rocio es muy alto; las condiciones son muy humedas y pueden sentirse incomodas.")
+})
