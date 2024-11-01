6edee14ccdff2c9925fa1e864737b790b187c8a1
VictoriaMarull <vmarull@mail.austral.edu.ar>
2024-10-31 22:35:11 -0300

    Arreglo algunas cosas

--- R/punto_rocio.R
+++ R/punto_rocio.R
@@ -10,20 +10,20 @@
 punto_rocio<- function(rocio_medio) {
   # Verificar que el argumento sea numérico
   if (!is.numeric(rocio_medio)) {
-    stop("El argumento debe ser un valor numérico que represente el punto de rocío.")
+    stop("El argumento debe ser un valor numérico que represente el punto de rocio.")
   }
 
   # Generar un mensaje basado en el valor del punto de rocío
   if (rocio_medio < 0) {
-    mensaje <- "El punto de rocío es muy bajo; el aire es muy seco."
+    mensaje <- "El punto de rocio es muy bajo; el aire es muy seco."
   } else if (rocio_medio >= 0 && rocio_medio <= 10) {
-    mensaje <- "El punto de rocío es bajo; el aire se siente fresco y seco."
+    mensaje <- "El punto de rocio es bajo; el aire se siente fresco y seco."
   } else if (rocio_medio > 10 && rocio_medio <= 15) {
-    mensaje <- "El punto de rocío es moderado; las condiciones son cómodas."
+    mensaje <- "El punto de rocio es moderado; las condiciones son comodas."
   } else if (rocio_medio > 15 && rocio_medio <= 20) {
-    mensaje <- "El punto de rocío es alto; el aire se siente un poco húmedo."
+    mensaje <- "El punto de rocío es alto; el aire se siente un poco humedo."
   } else {
-    mensaje <- "El punto de rocío es muy alto; las condiciones son muy húmedas y pueden sentirse incómodas."
+    mensaje <- "El punto de rocio es muy alto; las condiciones son muy humedas y pueden sentirse incomodas."
   }
 
   return(mensaje)
--- tests/testthat/test-punto_rocio.R
+++ tests/testthat/test-punto_rocio.R
@@ -1,3 +1,3 @@
 test_that("punto de rocio", {
-  expect_equal(object = punto_rocio(3), expected = "El punto de rocío es bajo; el aire se siente fresco y seco.")
+  expect_equal(object = punto_rocio(3), expected = "El punto de rocio es bajo; el aire se siente fresco y seco.")
 })
