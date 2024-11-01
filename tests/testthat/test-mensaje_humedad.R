0fc536d1bdc34ed83ca7d7ee90bb6efcc20e0d51
celestedallago <celedallago@gmail.com>
2024-11-01 00:16:10 -0300

    termino test de mensaje humedad

--- tests/testthat/test-mensaje_humedad.R
+++ tests/testthat/test-mensaje_humedad.R
@@ -1,3 +1,15 @@
 test_that("Mensaje humedad", {
   expect_equal(object = mensaje_humedad(30), expected = "La humedad es moderada.")
 })
+
+test_that("Mensaje humedad", {
+  expect_equal(object = mensaje_humedad(25), expected = "La humedad es baja.")
+})
+
+test_that("Mensaje humedad", {
+  expect_equal(object = mensaje_humedad(65), expected = "La humedad es alta.")
+})
+
+test_that("Mensaje humedad", {
+  expect_equal(object = mensaje_humedad(90), expected =  "La humedad es muy alta.")
+})
